e6b667fa7eb920c9919dde06624510f0984f8417
Ludwig Toresson <Ludwig_mu@hotmail.com>
2017-10-04 11:50:59 +0200

    Första commit

---
+++ paging.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+	menu();
+}
+
+void menu(){
+    printf("\nMenu:\n");
+    printf("1. Exekvera program1\n");
+    printf("2. Exekvera valfri bokstav\n");
+    printf("3. Skriv ut sidtabell och primärminne\n");
+    printf("4. Avsluta\n");
+	}
